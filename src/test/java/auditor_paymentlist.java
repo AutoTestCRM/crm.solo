17f83ec8a308defbbb19254c80d6ce0927cc0517
AutoTestSoloCRM <vitaliy.hrebeniuk@gmail.com>
2020-03-12 07:29:51 +0200

    commit

--- src/test/java/auditor_paymentlist.java
+++ src/test/java/auditor_paymentlist.java
@@ -3,13 +3,11 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class deals_payoutlist_fincontrol {
+public class auditor_paymentlist {
     public ChromeDriver driver;
     private Random random;
 
@@ -21,7 +19,7 @@ public class deals_payoutlist_fincontrol {
     }
 
     @Test
-    public void payout_list_fincontrol() throws InterruptedException {
+    public void auditor_paymentlist() throws InterruptedException {
 
         driver.get("https://solo-crm.com/#/");
 
@@ -44,16 +42,26 @@ public class deals_payoutlist_fincontrol {
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/a")).click();
         Thread.sleep(5000);
         //Choice Manager
-        driver.findElement(By.id("279")).click();
+        driver.findElement(By.id("425")).click();
         Thread.sleep(2000);
-        //Choice Finance
+
+        //Choice Payment List
         driver.findElement(By.xpath("//*[@id=\"navigation\"]/div[1]/div[2]/div/div/button")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div/a")).click();
-        //Wait payout List
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-        WebElement elem2 = driver.findElement(By.xpath("//*[@id=\"app\"]/div[7]/main/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div[6]/div/div[1]/div/table/tbody/tr[1]/td[4]/button"));
+        Thread.sleep(2000);
 
-        driver.quit();
+        //Click on the link
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[18]/main/div/div[2]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tbody/tr[1]/td[7]/a")).click();
+        Thread.sleep(10000);
+        driver.switchTo().window("Payment List");
+        Thread.sleep(5000);
+
+        //Click on the Comment
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[8]/main/div/div[2]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tbody/tr[1]/td[11]/span/span/button")).click();
+        //Waiting comment span
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement elem2 = driver.findElement(By.xpath("//*[@id=\"app\"]/div[8]/main/div/div[2]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tbody/tr[1]/td[11]/span/span/button/span"));
+
     }
 }
